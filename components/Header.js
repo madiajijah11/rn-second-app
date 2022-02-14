21583bc695d7974661cbcf690961548b95a2a206
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-15 02:03:45 +0800

    styling

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const Header = (props) => {
 const styles = StyleSheet.create({
   header: {
     width: "100%",
-    height: 90,
+    height: 100,
     paddingTop: 36,
     backgroundColor: "#f7287b",
     alignItems: "center",
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     color: "black",
-    fontSize: 18,
+    fontSize: 20,
   },
 });
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>StartGameScreen</Text>
+      <Text style={styles.title}>Please Input the Number</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputText} />
+        <View style={styles.buttonContainer}>
+          <Button style={{ color: "red" }} title="Reset" onPress={() => {}} />
+          <Button title="Start Game" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,8 +19,30 @@ const StartGameScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
+    padding: 10,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  inputText: {
+    width: "100%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 5,
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
 });
 
