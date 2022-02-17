544b32783add7cf4412269c17919435f2c6a2eeb
Dian Rahmadani <madiajijah7@gmail.com>
2022-02-18 02:00:35 +0800

    Create GameScreen.js

---
+++ screens/GameScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
